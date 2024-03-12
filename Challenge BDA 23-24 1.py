64fa09a9edcb97619ca8d6794cb372e27a1b6de5
Andreavisi1 <andreavisi1@gmail.com>
2024-03-12 15:52:17 +0100

    hhh

--- Challenge BDA 23-24 1.py
+++ Challenge BDA 23-24 1.py
@@ -1,14 +1,24 @@
 #gg
 #ciaooo
-print('Mario_hub')
-print("prova pull and push")
+#print('Mario_hub')
+#print("prova pull and push")
 
-ZIO PERA
+#ZIO PERA
 
-POTHENA
+#POTHENA
 
 #ciao
 
-adscfdwfqferwfqwf ZIO MELA
+#adscfdwfqferwfqwf ZIO MELA
 
-#ok
+##ok
+
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+sc = SparkContext.getOrCreate()
+spark = SparkSession(sc)
+entry_point = ‘hdfs://192.168.104.45:9000/user/amircoli/BDAchallenge2324’
+
+#df = spark.read.csv()
+
+print("Fanculo")
